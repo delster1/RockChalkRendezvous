422b35539b9b0721146a896fd94bd589415dc045
delster1 <delsterone@gmail.com>
2024-03-25 17:44:59 -0500

    working on encode and decode - got everything working except for calendar

--- prototype/calendar.hpp
+++ prototype/calendar.hpp
@@ -1,3 +1,7 @@
+// TODO: 
+// - refactor decode - move to timeblock and refactor calendar decode as folloiws
+// - 
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -7,7 +11,7 @@
 #include <algorithm>
 #include "definitions.hpp"
 #include "datetime_functions.hpp"
-
+#include "common_types.hpp"
 
 enum class RepeatType {
     NoRepeat,
@@ -20,16 +24,59 @@ enum class RepeatType {
 struct repeat_attr {
     RepeatType type;
     u16 duration_days;
+    inline repeat_attr(RepeatType type, u16 duration_days) : type(type), duration_days(duration_days) {}
+
     // u16 interval_days; - For Daily repetition - later 
 
     // Private constructor to force using build functions for clarity
     private:
-    repeat_attr(RepeatType repeatType, u16 duration_days)
-        : type(repeatType),duration_days(duration_days) {}
 
     public:
+    inline repeat_attr() : type(), duration_days() {}
+
     // Static build functions for different repeat types
+    std::string repeaAttrToString(RepeatType type) {
+        switch (type) {
+            case RepeatType::NoRepeat: return "NoRepeat";
+            case RepeatType::Daily: return "Daily";
+            case RepeatType::Weekly: return "Weekly";
+            case RepeatType::Monthly: return "Monthly";
+            case RepeatType::Yearly: return "Yearly";
+            default: return "Unknown";
+        }
+    }
     
+    static Option<RepeatType> stringToRepeatType(const std::string& str) {
+        if (str == "NoRepeat") return Option<RepeatType>::some(RepeatType::NoRepeat);
+        if (str == "Daily") return Option<RepeatType>::some(RepeatType::Daily);
+        if (str == "Weekly") return Option<RepeatType>::some(RepeatType::Weekly);
+        if (str == "Monthly") return Option<RepeatType>::some(RepeatType::Monthly);
+        if (str == "Yearly") return Option<RepeatType>::some(RepeatType::Yearly);
+
+        return Option<RepeatType>::none(); // Default case
+    }
+
+    std::string encode() {
+        std::ostringstream oss;
+        oss << repeaAttrToString(type) << " " << duration_days;
+        return oss.str();
+    }
+
+    static Option<repeat_attr> decode(std::istringstream& s) {
+        
+        std::string part;
+        std::getline(s, part, ' '); // Get RepeatType part
+        Option<RepeatType> type = stringToRepeatType(part);
+        std::getline(s, part, ' '); // Get duration_days part
+        u16 duration_days;
+        if (s >> duration_days && type.is_some()) {
+            return Option<repeat_attr>::some(repeat_attr::build_repeat_attr(type.unwrap(), duration_days));
+        }
+        else {
+            return Option<repeat_attr>::none();
+        }
+    }
+
     static repeat_attr build_repeat_attr(RepeatType repeatType,  u16 duration_days) {
         return {repeatType, duration_days};
     }
@@ -39,6 +86,42 @@ struct time_block {
     TimeAndDate start;
     TimeAndDate end;
     repeat_attr repeat_interval; // list of repeat attributes - determines multiple repeats - e.g: monthly and daily
+    
+    inline time_block(TimeAndDate start, TimeAndDate end, repeat_attr repeat_interval) : start(start), end(end), repeat_interval(repeat_interval) {}
+    public:
+    inline time_block() : start(), end(), repeat_interval() {}
+    std::string encode() {
+        std::ostringstream oss;
+        oss << repeat_interval.encode() << " "<< start.encode() << " " << end.encode();
+        return oss.str();
+    }
+
+    static Option<time_block> decode(std::istringstream& s) {
+        std::string repeatIntervalString;
+        std::string timeBlockStartString;
+        std::string timeBlockEndString;
+
+        std::getline(s, repeatIntervalString, ' ');
+        std::getline(s, timeBlockStartString, ' ');
+        std::getline(s, timeBlockEndString, ' ');
+
+        std::istringstream repeatIntervalStream(repeatIntervalString);
+        std::istringstream timeBlockStartStream(timeBlockStartString);
+        std::istringstream timeBlockEndStream(timeBlockEndString);
+
+        Option<repeat_attr> repeat = repeat_attr::decode(repeatIntervalStream);
+        Option<TimeAndDate> start = TimeAndDate::decode(timeBlockStartStream);
+        Option<TimeAndDate> end = TimeAndDate::decode(timeBlockEndStream);
+
+        if (repeat.is_some() && start.is_some() && end.is_some()) {
+            return Option<time_block>::some({start.unwrap(), end.unwrap(), repeat.unwrap()});
+        }
+        else {
+            return Option<time_block>::none();
+        }          
+    }
+
+
 
 };
 
@@ -101,7 +184,7 @@ struct calendar { // contains busy times only! - happy to discuss data structure
         std::vector<time_block> out;
         for (auto& busy_time : busy_times) {
             if (within(start, end, busy_time.start, busy_time.end)) {
-	            printf("%d %d %d:%d -> %d %d %d:%d\n",  busy_time.start.get_year(), busy_time.start.get_day_of_year(), busy_time.start.get_hour(), busy_time.start.get_minute(),  busy_time.end.get_year(), busy_time.end.get_day_of_year(), busy_time.end.get_hour(), busy_time.end.get_minute());
+	            // printf("%d %d %d:%d -> %d %d %d:%d\n",  busy_time.start.get_year(), busy_time.start.get_day_of_year(), busy_time.start.get_hour(), busy_time.start.get_minute(),  busy_time.end.get_year(), busy_time.end.get_day_of_year(), busy_time.end.get_hour(), busy_time.end.get_minute());
 
                 out.push_back(busy_time);
             }
@@ -118,4 +201,23 @@ struct calendar { // contains busy times only! - happy to discuss data structure
             }
         });
     }
+
+    calendar decode(std::istringstream& s) {
+        calendar decodedCal;
+        
+
+        return decodedCal;
+    }
+
+    
+    std::string encodeCalendar() {
+        std::ostringstream oss;
+        oss << busy_times.size() << "\n";
+        for(auto& busy_time : busy_times) {
+
+            oss << busy_time.encode() << "\n";
+        } 
+        return oss.str();
+    }
 };
+
--- prototype/calendar/main.cpp
+++ prototype/calendar/main.cpp
@@ -1,3 +1,5 @@
+
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -7,118 +9,8 @@
 #include <algorithm>
 #include "../definitions.hpp"
 #include "../datetime_functions.hpp"
-
-
-enum class RepeatType {
-    NoRepeat,
-    Daily,
-    Weekly,
-    Monthly,
-    Yearly
-};
-
-struct repeat_attr {
-    RepeatType type;
-    u16 duration_days;
-    // u16 interval_days; - For Daily repetition - later 
-
-    // Private constructor to force using build functions for clarity
-    private:
-    repeat_attr(RepeatType repeatType, u16 duration_days)
-        : type(repeatType),duration_days(duration_days) {}
-
-    public:
-    // Static build functions for different repeat types
-    
-    static repeat_attr build_repeat_attr(RepeatType repeatType,  u16 duration_days) {
-        return {repeatType, duration_days};
-    }
-};
-
-struct time_block { 
-    TimeAndDate start;
-    TimeAndDate end;
-    repeat_attr repeat_interval; // list of repeat attributes - determines multiple repeats - e.g: monthly and daily
-
-};
-
-struct calendar { // contains busy times only! - happy to discuss data structure for this tho
-    std::vector<time_block> busy_times;
-    /* will probably contain functions for:
-        - getting busy times for a certain day - done
-        - getting busy times for a certain time range - done
-        - removing busy times - do we really need this - we can just find in the list and remove at index?
-        - comparison function for two times to sort - done
-        - etc.
-    */
-    public:
-    bool is_valid_time(time_block time) {
-        if (time.start >= time.end ) {
-            return false;
-        }
-        for ( auto& busy_time : busy_times) {
-            // Check for any kind of overlap between the new time block and existing busy times
-            if (overlaps(time.start, time.end, busy_time.start, busy_time.end)) {
-                return false; // Found an overlap, time is not valid
-            }
-        }
-        return true;
-    }
-
-    bool overlaps( TimeAndDate& start1,  TimeAndDate& end1,  TimeAndDate& start2,  TimeAndDate& end2) {
-        // Check if one time block starts before the other ends and ends after the other starts (inverse of having no overlap)
-        return start1 <= end2 && start2 <= end1;
-    }
-
-    bool within( TimeAndDate& start1,  TimeAndDate& end1,  TimeAndDate& start2,  TimeAndDate& end2) {
-        return start1 <= start2 && end2 <= end1;
-    }
-
-    void add_time(time_block to_add) {
-        if (is_valid_time(to_add)) {
-            busy_times.push_back(to_add);
-        }
-        else {
-            std::cout << "Invalid time!\n";
-        }
-    }
-
-    std::vector<time_block> get_busy_times_in_day(TimeAndDate& day) {
-        // this function compares each obj in the list of busy times to this TimeAndDate object day. It checks both start and end to return times in a specific day only
-        std::vector<time_block> out;
-        for (auto& busy_time : busy_times) {
-            if (busy_time.start.get_day_of_year() == day.get_day_of_year() && busy_time.start.get_year() == day.get_year() && busy_time.end.get_day_of_year() == day.get_day_of_year()){
-	            // printf("%d %d %d:%d -> %d %d %d:%d\n",  busy_time.start.get_year(), busy_time.start.get_day_of_year(), busy_time.start.get_hour(), busy_time.start.get_minute(),  busy_time.end.get_year(), busy_time.end.get_day_of_year(), busy_time.end.get_hour(), busy_time.end.get_minute());
-                
-                out.push_back(busy_time);
-            } 
-        }
-        return out;
-    }
-
-    std::vector<time_block> get_busy_times_in_range(TimeAndDate& start, TimeAndDate& end) {
-        // this function compares each obj in the list of busy times to this TimeAndDate object day. It checks both start and end to return times in a specific day only
-        std::vector<time_block> out;
-        for (auto& busy_time : busy_times) {
-            if (within(start, end, busy_time.start, busy_time.end)) {
-	            printf("%d %d %d:%d -> %d %d %d:%d\n",  busy_time.start.get_year(), busy_time.start.get_day_of_year(), busy_time.start.get_hour(), busy_time.start.get_minute(),  busy_time.end.get_year(), busy_time.end.get_day_of_year(), busy_time.end.get_hour(), busy_time.end.get_minute());
-
-                out.push_back(busy_time);
-            }
-        }
-        return out;
-    }
-
-    void sort_busy_times() {
-        std::sort(busy_times.begin(), busy_times.end(), []( time_block& a,  time_block& b) {
-            if (a.start != b.start) {
-                return a.start < b.start;
-            } else {
-                return a.end < b.end; // If starts are equal, compare ends
-            }
-        });
-    }
-};
+#include "../common_types.hpp"
+#include "../calendar.hpp"
 
 int main () {
     // this main function is horribly messy - it just tests all of the functions above 
@@ -130,7 +22,6 @@ int main () {
     // Adding a non-repeating time block
     time_block nonRepeatingBlock = {startTime, endTime, repeat_attr::build_repeat_attr(RepeatType::NoRepeat, 0)};
     my_cal.add_time(nonRepeatingBlock);
-	printf("%d %d %d:%d -> %d %d %d:%d\n",  my_cal.busy_times[0].start.get_year(), my_cal.busy_times[0].start.get_day_of_year(), my_cal.busy_times[0].start.get_hour(), my_cal.busy_times[0].start.get_minute(),  my_cal.busy_times[0].end.get_year(), my_cal.busy_times[0].end.get_day_of_year(), my_cal.busy_times[0].end.get_hour(), my_cal.busy_times[0].end.get_minute());
 
     // Adding a daily repeating time block
     time_block dailyRepeatingBlock = {startTime, endTime, repeat_attr::build_repeat_attr(RepeatType::Daily, 100)};
@@ -161,6 +52,9 @@ int main () {
 
     my_cal.sort_busy_times();
 
-
+    std::string decoded_cal = my_cal.encodeCalendar();
+    std::cout << decoded_cal; 
+    std::istringstream decodedCalStream(decoded_cal);
+    my_cal.decode(decodedCalStream);
     return 0;
 }
