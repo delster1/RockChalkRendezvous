2db26c7c6b39d83d4da80496dcdbf0eb3cb7e309
delster1 <delsterone@gmail.com>
2024-04-16 15:16:08 -0500

    got test ui working, iterating through a calendar and drawing populated instances

--- prototype/ui/main.cpp
+++ prototype/ui/main.cpp
@@ -28,25 +28,33 @@ int main() {
 
     // Create grid
     const int minutes_intervals = 48; // 15-minute intervals in a day
-    const int days_of_week = 7;  // Days of the week
+    const int days_of_week = 8;  // Days of the week
 
     // Print header
     attron(COLOR_PAIR(1));
-    mvprintw(0, 0, "%s %s %s %s %s %s %s", DAY_NAMES[0], DAY_NAMES[1], DAY_NAMES[2], DAY_NAMES[3], DAY_NAMES[4], DAY_NAMES[5], DAY_NAMES[6]);
+    mvprintw(0, 0, "TIME %s %s %s %s %s %s %s", DAY_NAMES[0], DAY_NAMES[1], DAY_NAMES[2], DAY_NAMES[3], DAY_NAMES[4], DAY_NAMES[5], DAY_NAMES[6]);
     attroff(COLOR_PAIR(1));
 
     // Print grid
     int y = 2; // Start from the second row
     for (int minutes_interval = 0; minutes_interval < minutes_intervals; ++minutes_interval) {
         for (int day_of_week = 0; day_of_week < days_of_week; ++day_of_week) {
+            
             int x = day_of_week * 20; // 4 spaces per column for spacing
+            
             int time_of_day = minutes_interval * 30; // Time in minutes from midnight
-            TimeAndDate currentTime = TimeAndDate::build(time_of_day, day_of_week + 1, 2024);
-            bool is_busy = myCalendar.is_time_block_busy(currentTime);
-            // Check if current time block is busy
-            attron(COLOR_PAIR(myCalendar.is_time_block_busy(currentTime) ? 1 : 2));
-            mvprintw(y + minutes_interval, x, "%s", is_busy ? "true" : "false");
-            attroff(COLOR_PAIR(myCalendar.is_time_block_busy(currentTime) ? 1 : 2));
+            if( day_of_week == 0){
+                mvprintw(y + minutes_interval, x, "%4d:%4d",(time_of_day / 60) + 1, time_of_day % 60 );
+            }
+            else {
+                TimeAndDate currentTime = TimeAndDate::build(time_of_day, day_of_week , 2024);
+                bool is_busy = myCalendar.is_time_block_busy(currentTime);
+                // Check if current time block is busy
+                attron(COLOR_PAIR(myCalendar.is_time_block_busy(currentTime) ? 1 : 2));
+                mvprintw(y + minutes_interval, x, "%s", is_busy ? "#############" : "-------------");
+                attroff(COLOR_PAIR(myCalendar.is_time_block_busy(currentTime) ? 1 : 2));
+            }
+            
         }
     }
 
